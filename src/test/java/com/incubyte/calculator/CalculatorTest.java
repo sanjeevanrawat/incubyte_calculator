f57c78585d84a5a7261f87aac1e4b7a046a91b05
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-07-13 02:03:10 +0530

    init project

--- src/test/java/com/incubyte/calculator/CalculatorTest.java
+++ src/test/java/com/incubyte/calculator/CalculatorTest.java
@@ -6,54 +6,8 @@ import org.junit.*;
 public class CalculatorTest {
 
 	@Test
-	public void shouldReturnZeroOnEmptyString(){
+	public void test(){
 		assertEquals(0, Calculator.add(""));
 	}
-	@Test
-	public void shouldReturnNumberOnNumber(){
-		assertEquals(1, Calculator.add("1"));
-	}
-	@Test
-	public void shouldReturnSumOnTwoNumbersDelimitedByComma(){
-		assertEquals(3, Calculator.add("1,2"));
-	}
-	@Test
-	public void shouldReturnSumOnMultipleNumbers(){
-		assertEquals(6, Calculator.add("1,2,3"));
-	}
-	@Test
-	public void shouldAcceptNewLineAsValidDelimiter(){
-		assertEquals(6, Calculator.add("1,2\n3"));
-	}
-	@Test
-	public void shouldAcceptCustomDelimiterSyntax(){
-		assertEquals(3, Calculator.add("//;\n1;2"));
-	}
-	@Test
-	public void customDelimiterCouldAlsoBeARegExpSpecialChar(){
-		assertEquals(3, Calculator.add("//.\n1.2"));
-	}
-	@Test
-	public void shouldRaiseExceptionOnNegatives(){
-		try{
-			Calculator.add("-1,2,3");
-			fail("Exception expected");
 
-		}catch(RuntimeException ex){
-
-		}
-	}
-	@Test
-	public void exceptionMessageShouldContainTheNegativeNumber(){
-		try{
-			Calculator.add("-1,-2,3");
-			fail("Exception expected");
-		}catch(RuntimeException ex){
-			assertEquals("negatives not allowed: -1, -2" , ex.getMessage());
-		}
-	}
-	@Test
-	public void getCount(){
-		assertEquals(9, Calculator.GetCalledCount());
-	}
 }
