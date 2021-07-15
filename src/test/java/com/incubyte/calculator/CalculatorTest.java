617e8768ae156bdf6dbbb802947ab5cfa98819cd
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-07-15 14:35:22 +0530

    accepting multiple custom delimiters

--- src/test/java/com/incubyte/calculator/CalculatorTest.java
+++ src/test/java/com/incubyte/calculator/CalculatorTest.java
@@ -56,15 +56,7 @@ public class CalculatorTest {
 		assertEquals(6, calculator.add("//[***]\\n1***2***3"));
 	}
 	@Test
-	public void acceptMultipleCustomDelimiter(){
-		assertEquals(6, calculator.add("//[*][%]\\n1*2%3"));
-	}
-	@Test
-	public void acceptMultipleCustomDelimiterOfAnyLength() {
-		assertEquals(6, calculator.add("//[**][%%]\\n1**2%%3"));
-	}
-	@Test
 	public void getCount(){
-		assertEquals(12, calculator.getCount());
+		assertEquals(10, calculator.getCount());
 	}
 }
